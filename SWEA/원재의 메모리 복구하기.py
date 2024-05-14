19de8413aa53ebc953af55a52940b3cde91be894
JUUUUN <wnsrl7250@naver.com>
2024-05-14 22:52:21 +0900

    SWEA 원재의 메모리 복구하기

---
+++ SWEA/원재의 메모리 복구하기.py
@@ -0,0 +1,16 @@
+T=int(input())
+
+for tc in range(1,T+1):
+    n = list(input())
+    arr = list(0 for i in range(len(n)))
+    result=0
+
+    for i in range(len(arr)):
+        if int(n[i]) != arr[i]:
+            for j in range(i,len(arr)):
+                arr[j] = int(n[i])
+            result += 1
+        else:
+            continue
+    
+    print(f"#{tc} {result}")
