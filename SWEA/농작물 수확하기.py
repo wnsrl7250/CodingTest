9e8fe198720ccd4647b773ea72b354001ec81a04
JUUUUN <wnsrl7250@naver.com>
2024-05-14 18:31:14 +0900

    SWEA 농작물 수확

---
+++ SWEA/농작물 수확하기.py
@@ -0,0 +1,14 @@
+T = int(input())
+
+for tc in range(1,T+1):
+    N = int(input())
+    K = [list(map(int, input())) for _ in range(N)]
+    answer = 0
+    mid = (N-1)//2
+    for i in range(N):
+        if i<=mid:
+            answer += sum(K[i][mid-i:mid+1+i])
+        else:
+            answer += sum(K[i][i-mid:mid-i])
+    
+    print(f'#{tc} {answer}')
